e11a06324ff44b7e97611ca169aa3496f6b70bce
Bailey Le <baileyble@gmail.com>
2020-07-17 21:22:14 -0400

    Updates

---
+++ precache-manifest.47a67e0e39e89c0b4a6635ce2f2d96dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a25d1870279a92286655a8041a8ef6b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "70c75ff09cddf372b24b",
+    "url": "/my-portfolio/static/css/main.b6ff0d2e.chunk.css"
+  },
+  {
+    "revision": "90f2f4e81460eb9433cb",
+    "url": "/my-portfolio/static/js/2.0c607222.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.0c607222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c75ff09cddf372b24b",
+    "url": "/my-portfolio/static/js/main.9759c703.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
